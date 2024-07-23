83effa12419fe64888a140f625c30d007308bba0
ueda mai <uedamai813@gmail.com>
2024-07-23 21:51:12 +0900

    suspense 4th

--- app/components/SearchField.tsx
+++ app/components/SearchField.tsx
@@ -7,7 +7,7 @@ import { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import { Suspense } from 'react';
 
-export default function SearchField() {
+function Search() {
   const searchParams = useSearchParams();
   const { replace } = useRouter();
   const [query, setQuery] = useState(searchParams.get('q')?.toString() || '');
@@ -30,29 +30,35 @@ export default function SearchField() {
 
   return (
     <>
-      <Suspense fallback={<div>Loading search field...</div>}>
-        <div className="card-body p-4 mb-8 rounded-xl h-fit">
-          <h2 className="font-kiwi text-xl text-center mb-4">Search</h2>
-          <form onSubmit={handleFormSubmit}>
-            <div className="flex w-full">
-              <input
-                type="text"
-                placeholder="Search..."
-                value={query}
-                onChange={handleInputChange}
-                className="w-[126px] flex-grow p-2 border rounded-l-lg "
-              />
-              <button
-                type="submit"
-                disabled={!query}
-                className="w-[60px] md:w-[42px] p-2 bg-rose-300 text-white rounded-r-lg disabled:bg-neutral-300 grid place-content-center"
-              >
-                <FaSearch />
-              </button>
-            </div>
-          </form>
-        </div>
-      </Suspense>
+      <div className="card-body p-4 mb-8 rounded-xl h-fit">
+        <h2 className="font-kiwi text-xl text-center mb-4">Search</h2>
+        <form onSubmit={handleFormSubmit}>
+          <div className="flex w-full">
+            <input
+              type="text"
+              placeholder="Search..."
+              value={query}
+              onChange={handleInputChange}
+              className="w-[126px] flex-grow p-2 border rounded-l-lg "
+            />
+            <button
+              type="submit"
+              disabled={!query}
+              className="w-[60px] md:w-[42px] p-2 bg-rose-300 text-white rounded-r-lg disabled:bg-neutral-300 grid place-content-center"
+            >
+              <FaSearch />
+            </button>
+          </div>
+        </form>
+      </div>
     </>
+  );
+}
+
+export default function SearchField() {
+  return (
+    <Suspense>
+      <Search />
+    </Suspense>
   );
 }
